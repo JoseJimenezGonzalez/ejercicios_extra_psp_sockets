2bdefd068499858a2035ce7d7d83339842c9a549
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-31 18:53:05 +0100

    commit ok

--- src/ej3/cliente/ClienteSocketStream.java
+++ src/ej3/cliente/ClienteSocketStream.java
@@ -1,12 +1,17 @@
 package ej3.cliente;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClienteSocketStream {
     public static void main(String[] args) throws IOException {
+
+        Scanner teclado = new Scanner(System.in);
+
         System.out.println("Creando el socket cliente");
         Socket cliente = new Socket();
 
@@ -16,12 +21,19 @@ public class ClienteSocketStream {
 
         //Creamos el flujo de salida
         OutputStream salida = cliente.getOutputStream();
+        InputStream entrada = cliente.getInputStream();
 
-        //Creamos el mensaje
-        String mensaje = "ESTE ES EL MENSAJE";
-        salida.write(mensaje.getBytes());
-
-        System.out.println("Mensaje enviado");
+        //Introducir la letra
+        System.out.println("Introduce una letra");
+        String letraUsuario = teclado.next();
+        //Pasamos a mandar la letra al servidor
+        salida.write(letraUsuario.getBytes());
+        //Recibimos la palabra que empieza por esa letra
+        System.out.println("La palabra que empiza por esa letra es: ");
+        byte[] mensaje = new byte[50];
+        entrada.read(mensaje);
+        String palabraResultado = new String(mensaje).trim();
+        System.out.println(palabraResultado);
         System.out.println("Cerrando el socket Cliente");
         cliente.close();
         System.out.println("Conexion socket Cliente cerrada");
--- src/ej3/servidor/ServidorSocketStream.java
+++ src/ej3/servidor/ServidorSocketStream.java
@@ -6,9 +6,43 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServidorSocketStream {
     public static void main(String[] args) {
+        Map<String, String> mapaPalabras = new HashMap<>();
+
+        // Agregar datos al HashMap
+        mapaPalabras.put("a ", "Amigo");
+        mapaPalabras.put("b", "Bicicleta");
+        mapaPalabras.put("c", "Casa");
+        mapaPalabras.put("d", "Dedo");
+        mapaPalabras.put("e", "Elefante");
+        mapaPalabras.put("f", "Familia");
+        mapaPalabras.put("g", "Gato");
+        mapaPalabras.put("h", "Helado");
+        mapaPalabras.put("i", "Isla");
+        mapaPalabras.put("j", "Juego");
+        mapaPalabras.put("k", "Koala");
+        mapaPalabras.put("l", "Lapicero");
+        mapaPalabras.put("m", "Mesa");
+        mapaPalabras.put("n", "Nube");
+        mapaPalabras.put("ñ", "Ñoqui");
+        mapaPalabras.put("o", "Ola");
+        mapaPalabras.put("p", "Perro");
+        mapaPalabras.put("q", "Queso");
+        mapaPalabras.put("r", "React");
+        mapaPalabras.put("s", "Sol");
+        mapaPalabras.put("t", "Tomate");
+        mapaPalabras.put("u", "Uva");
+        mapaPalabras.put("v", "Ventana");
+        mapaPalabras.put("w", "Waffles");
+        mapaPalabras.put("x", "Xenomorfo");
+        mapaPalabras.put("y", "Yogur");
+        mapaPalabras.put("z", "Zorro");
+
+
         try {
             System.out.println("Creando socket Servidor");
             ServerSocket servidor = new ServerSocket();
@@ -26,8 +60,13 @@ public class ServidorSocketStream {
                 InputStream entrada = socketCliente.getInputStream();
                 OutputStream salida = socketCliente.getOutputStream();
 
-                byte[] mensaje = new byte[1000];
+                //Leemos la letra que nos manda el cliente
+                byte[] mensaje = new byte[10];
                 entrada.read(mensaje);
+                String letraCliente = new String(mensaje).trim();
+                String palabraAsociadaLetra = mapaPalabras.get(letraCliente);
+                //Mandamos la palabra que nos ha devulto el hash map
+                salida.write(palabraAsociadaLetra.getBytes());
 
                 System.out.println("Mensaje recibido " + new String(mensaje).trim());
 
