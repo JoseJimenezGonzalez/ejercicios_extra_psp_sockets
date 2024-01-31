f45924094e2af720410b7777331ed059bf405eb6
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-31 18:27:29 +0100

    commit ok

--- src/ej2/cliente/ClienteSocketStream.java
+++ src/ej2/cliente/ClienteSocketStream.java
@@ -1,12 +1,15 @@
 package ej2.cliente;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClienteSocketStream {
     public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
         System.out.println("Creando el socket cliente");
         Socket cliente = new Socket();
 
@@ -14,14 +17,28 @@ public class ClienteSocketStream {
         InetSocketAddress dircServidor = new InetSocketAddress ("localhost", 55555);
         cliente.connect(dircServidor);
 
-        //Creamos el flujo de salida
+        //Creamos los flujos
         OutputStream salida = cliente.getOutputStream();
+        InputStream entrada = cliente.getInputStream();
 
-        //Creamos el mensaje
-        String mensaje = "ESTE ES EL MENSAJE";
-        salida.write(mensaje.getBytes());
+        //Primero va el mensaje de cuantas palabras se van a concatenar
+        int palabrasEnviar = 5;
+        String mensajeNumeroPalabras = String.valueOf(palabrasEnviar);
+        salida.write(mensajeNumeroPalabras.getBytes());
+        //Se van a enviar las palabras una a una
+        for (int i = 0; i < palabrasEnviar ; i++){
+            System.out.println("Introduce palabra");
+            String palabra = teclado.next();
+            salida.write(palabra.getBytes());
+        }
 
-        System.out.println("Mensaje enviado");
+        //Recibimos las palabras concatenadas
+        byte[] mensaje = new byte[500];
+        entrada.read(mensaje);
+        String resultado = new String(mensaje).trim();
+        //Mostramos las palabras concatenadas
+        System.out.println("El resultado de las palabras concatenadas.");
+        System.out.println(resultado);
         System.out.println("Cerrando el socket Cliente");
         cliente.close();
         System.out.println("Conexion socket Cliente cerrada");
--- src/ej2/servidor/ServidorSocketStream.java
+++ src/ej2/servidor/ServidorSocketStream.java
@@ -26,10 +26,25 @@ public class ServidorSocketStream {
                 InputStream entrada = socketCliente.getInputStream();
                 OutputStream salida = socketCliente.getOutputStream();
 
-                byte[] mensaje = new byte[1000];
+                //Recibimos el numero de palabras
+                byte[] mensaje = new byte[50];
                 entrada.read(mensaje);
+                System.out.println("Se van a enviar " + new String(mensaje).trim() + " palabras.");
+                int numeroPalabras = Integer.valueOf(new String(mensaje).trim());
+                //Recibimos las palabras
+                StringBuilder resultado = new StringBuilder();
+                //Bucle que nos dice cuando parar segun el numero de palabras
+                for (int i = 0; i < numeroPalabras; i++){
+                    byte[] palabraBytes = new byte[50];
+                    entrada.read(palabraBytes);
+                    resultado.append(new String(palabraBytes).trim() + " ");
+                }
+                //Mostramos las palabras concatenadas
+                System.out.println("El resultado de las palabras concatenadas.");
+                System.out.println(resultado);
+                //Mandamos el resultado al cliente
+                salida.write(resultado.toString().getBytes());
 
-                System.out.println("Mensaje recibido " + new String(mensaje).trim());
 
                 System.out.println("Cerramos conexion SUBSOCKET del cliente");
                 socketCliente.close();
