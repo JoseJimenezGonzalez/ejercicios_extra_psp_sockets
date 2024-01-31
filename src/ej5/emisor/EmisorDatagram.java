d5c0c80999aa435893495620643d11b37c1f1ee3
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-31 19:41:22 +0100

    commit ok

---
+++ src/ej5/emisor/EmisorDatagram.java
@@ -0,0 +1,46 @@
+package ej5.emisor;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+
+public class EmisorDatagram {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Creamos el DatagramSocket 1");
+
+        InetSocketAddress dirApi1 = new InetSocketAddress("localhost", 55555);
+        DatagramSocket socketApi1 = new DatagramSocket(dirApi1);
+
+
+        System.out.println("Mensaje para el receptor");
+
+        String mensaje = teclado.nextLine();
+
+        InetAddress dirRecep = InetAddress.getByName("localhost");
+        DatagramPacket datagrama = new DatagramPacket(mensaje.getBytes(), mensaje.getBytes().length, dirRecep, 55556);
+
+        socketApi1.send(datagrama);
+
+        System.out.println("Mensaje enviado");
+
+        System.out.println("Esperamos respuesta del DatagramSocket 2");
+
+        byte [] respuesta = new byte[50];
+        DatagramPacket datagrama_resp = new DatagramPacket(respuesta, respuesta.length);
+
+        socketApi1.receive(datagrama_resp);
+
+        System.out.println("Respuesta recibida: " + new String(respuesta));
+        System.out.println("Cerrado el DatagramSocket 1");
+
+        socketApi1.close();
+
+    }
+}
---
+++ src/ej6/receptor/ReceptorDatagram.java
@@ -0,0 +1,44 @@
+package ej6.receptor;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+public class ReceptorDatagram {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Creamos el DatagramSocket 2");
+
+        InetSocketAddress dirApi2 = new InetSocketAddress("localhost", 55556);
+        DatagramSocket socketApi2 = new DatagramSocket(dirApi2);
+
+
+        System.out.println("Esperando respuesta DatagramSocket 1");
+
+        byte[] respuesta = new byte[50];
+        DatagramPacket datagrama = new DatagramPacket(respuesta, respuesta.length);
+        socketApi2.receive(datagrama);
+        String strRespuesta = new String(respuesta).trim();
+        //condicional segun contenga una palabra u otra
+        String mensajeSegunPalabra = "";
+        if (strRespuesta.contains("Hola")) {
+            mensajeSegunPalabra = "Hola que tal?";
+        } else if (strRespuesta.contains("Adiós")) {
+            mensajeSegunPalabra = "Hasta luego";
+        }else {
+            mensajeSegunPalabra = "He recibido tu mensaje";
+        }
+        System.out.println("Enviamos respuesta a DatagramSocket 1");
+        DatagramPacket mensajeResp = new DatagramPacket(mensajeSegunPalabra.getBytes(),
+                mensajeSegunPalabra.getBytes().length,datagrama.getAddress(),datagrama.getPort());
+        socketApi2.send(mensajeResp);
+        System.out.println("Mensaje de respuesta enviado");
+        System.out.println("Cerramos el socket");
+        socketApi2.close();
+        System.out.println("Terminamos en Api2");
+
+
+    }
+}
